77eb2355765ec15708f8492fe5e01980f64628cc
kaifsindhi <kaifsindhI@gmail.com>
2022-08-06 20:56:03 -0500

    edit html

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
       <header className="App-header">
         <h1>hello world</h1>
         <p>hosting a website is very easy with GitHub pages</p>
+        <p>i made an edit</p>
       </header>
     </div>
   );
