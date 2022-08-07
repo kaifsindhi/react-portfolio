878e127716f1c29833a3c9b142c288e9c952159b
kaifsindhi <kaifsindhI@gmail.com>
2022-08-06 22:00:00 -0500

    add material-ui, update favicon, update title

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import "./App.css";
+import MainPage from "./pages/Main";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>hello world</h1>
-        <p>hosting a website is very easy with GitHub pages</p>
-        <p>i made an edit</p>
-      </header>
+    <div>
+        <MainPage/>
     </div>
   );
 }
---
+++ src/pages/Main.js
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+export default function MainPage() {
+  return (
+    <div>
+      <h1>hello world</h1>
+      <p>hosting a website is very easy with GitHub pages</p>
+      <p>i made an edit</p>
+    </div>
+  );
+}
